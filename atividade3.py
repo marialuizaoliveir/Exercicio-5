3b0feaa38bcf3c8a7013bbf25f34773923cea148
marialuizaoliveir <marialuizaoliveirasantos165@gmail.com>
2024-10-16 20:05:24 -0300

    Add files via upload

---
+++ atividade3.py
@@ -0,0 +1,7 @@
+numero=int(input("Iforme o número"))
+if numero>0:
+ print("Número positivo")
+elif numero<0:
+  print("Número negativo")
+else:
+  print("Número é zero")
---
+++ atividades2.py
@@ -0,0 +1,6 @@
+numero1=int(input("Iforme o primeiro número"))
+numero2=int(input("Informe o segundo número"))
+if numero1==2:
+    print("Números iguais")
+else:
+    print("números diferentes")    
